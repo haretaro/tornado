d943003a4f22f08ba1803dd55e8e23994550415b
haretaro <frontend.1000@gmail.com>
2015-12-01 17:33:02 +0900

    クラス変数でインスタンス間で値を共有してみた

--- websock_test.py
+++ websock_test.py
@@ -9,6 +9,12 @@ class MainHandler(tornado.web.RequestHandler):
 
 class SendWebSocket(tornado.websocket.WebSocketHandler):
 
+  num = 0
+
+  @classmethod
+  def update_num(cls):
+    cls.num += 1
+
   def check_origin(self, origin):
     return True
 
@@ -16,14 +22,20 @@ class SendWebSocket(tornado.websocket.WebSocketHandler):
     self.i = 0
     self.callback = PeriodicCallback(self._send_message, 400)
     self.callback.start()
+    self.update_num()
     print('WebSocket opendやで')
 
   def on_message(self, message):
     print(message)
 
+  @classmethod
+  def get_num(cls):
+    return cls.num
+
   def _send_message(self):
     self.i += 1
-    self.write_message(str(self.i))
+    self.i += 1
+    self.write_message(str(self.i)+' num='+str(self.get_num()))
 
   def on_close(self):
     self.callback.stop()
@@ -35,5 +47,6 @@ app = tornado.web.Application([
 ])
 
 if __name__ == '__main__':
+  i = 0
   app.listen(8888)
   tornado.ioloop.IOLoop.instance().start()
