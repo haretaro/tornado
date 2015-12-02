08688dcf314496a4776c371a34d395ab93858dd9
haretaro <frontend.1000@gmail.com>
2015-12-02 14:10:35 +0900

    added jsay.html, jsay.py

--- jsay.py
+++ jsay.py
@@ -5,16 +5,22 @@ from tornado.ioloop import PeriodicCallback
 class MainPage(tornado.web.RequestHandler):
 
     def get(self):
-        self.render('index.html')
+        self.render('jsay.html')
 
 class SendWebSocket(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        print('connected')
+
+    def on_close(self):
+        print('closed')
 
     def on_message(self, message):
         print(message)
 
 app = tornado.web.Application([
-    (r'/',Mainpage),
-    (r'/socket',SendWebSocket)
+    (r'/',MainPage),
+    (r'/websocket',SendWebSocket)
     ])
 
 if __name__ == '__main__':
